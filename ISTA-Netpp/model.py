781ed7cb79f5cd721553826a3d76083817a315c9
alphayuan <171280059@qq.com>
2022-04-07 23:19:49 +0800

    实现了ISTANetpp

---
+++ ISTA-Netpp/model.py
@@ -0,0 +1,113 @@
+import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class ConditionModule(nn.Module):
+    def __init__(self, layerNum):
+        super(ConditionModule, self).__init__()
+        model = [
+            nn.Linear(1, 32, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, 32, bias=True),
+            nn.ReLU(inplace=True),
+            nn.Linear(32, layerNum * 2, bias=True),
+            nn.Softplus(),
+        ]
+        self.model = nn.Sequential(*model)
+
+    def forward(self, x):
+        x = self.model(x)
+        num = int(x.shape[1] / 2)
+        return x[0, 0:num], x[0, num:]
+
+
+class ResidualBlock(nn.Module):
+    def __init__(self, nf=64):
+        super(ResidualBlock, self).__init__()
+        self.conv1 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+        self.act = nn.ReLU(inplace=True)
+        self.conv2 = nn.Conv2d(nf, nf, 3, 1, 1, bias=True)
+
+    def forward(self, x):
+        x0 = x[0]
+        cond = x[1]
+        out = self.act(self.conv1(x0))
+        out = self.conv2(out)
+        return x0 + out, cond
+
+
+def PhiTPhi(x, PhiWeight, PhiTWeight):
+    tmp = F.conv2d(x, PhiWeight, bias=None, stride=128, padding=0)
+    tmp = F.conv2d(tmp, PhiTWeight, bias=None, padding=0)
+    tmp = F.pixel_shuffle(tmp, upscale_factor=128)
+    return tmp
+
+
+class DGDMBlock(nn.Module):
+    def __init__(self):
+        super(DGDMBlock, self).__init__()
+
+    def forward(self, x, y, rho, PhiWeight, PhiTWeight):
+        # PhiWeight = Phi.contiguous().view(-1, 1, 33, 33)
+        # PhiTWeight = Phi.transpose().contiguous().view(-1, 1, 1, 1)
+
+        r = x - rho * PhiTPhi(x, PhiWeight, PhiTWeight)
+        r = r + rho * y
+        return r
+
+
+class DPMMBlock(nn.Module):
+    def __init__(self, nf=32):
+        super(DPMMBlock, self).__init__()
+        self.extr = nn.Conv2d(2, nf, 3, 1, 1, bias=True)
+        self.rb1 = ResidualBlock(nf)
+        self.rb2 = ResidualBlock(nf)
+        self.recon = nn.Conv2d(nf, 1, 3, 1, 1, bias=True)
+
+    def forward(self, r, sigma):
+        m = torch.Tensor.repeat(r.shape[0], 1, r.shape[2], r.shape[3])
+        in0 = torch.cat((r, m), 1)
+        out = self.extr(in0)
+        out, _ = self.rb1([out, None])
+        out, _ = self.rb2([out, None])
+        out = self.recon(out)
+        return r + out
+
+
+class ISTA_Netpp(nn.Module):
+    def __init__(self, layerNum, n_output):
+        super(ISTA_Netpp, self).__init__()
+        self.layerNum = layerNum
+        self.n_output = n_output
+
+        self.cm = ConditionModule(layerNum)
+        self.blocks = []
+        for i in range(layerNum):
+            self.blocks.append([DGDMBlock(), DPMMBlock()])
+
+    def forward(self, batch_y, gamma, Phi, n_input):
+        rho_k, sigma_k = self.cm(gamma)
+
+        PhiWeight = Phi.contiguous().view(n_input, 1, 128, 128)
+        PhiTWeight = Phi.transpose().contiguous().view(self.n_ouput, n_input, 1, 1)
+        # y = F.conv2d(batch_x, PhiWeight, stride=33, padding=0, bias=None)
+        # y = F.conv2d(y, PhiTWeight, padding=0, bias=None)
+        # y = nn.PixelShuffle(33)(y)
+        # x = y
+        x = batch_y
+        y = batch_y
+
+        for i in range(self.layerNum):
+            r = self.blocks[i][0](x, y, rho_k[i], PhiWeight, PhiTWeight)
+            x = self.blocks[i][1](r, sigma_k[i])
+
+        return x
+
+
+if __name__ == '__main__':
+    CM = ConditionModule(20)
+    gamma = torch.from_numpy(np.arange(0.1, 0.5, 0.1, dtype=np.float32).reshape(1, -1))
+    print(gamma[:, 0:1])
+    print(CM(gamma[:, 0:1]))
---
+++ ISTA-Netpp/train.py
@@ -0,0 +1,166 @@
+import os
+import time
+from argparse import ArgumentParser
+
+import scipy.io as sio
+# from skimage.measure import compare_ssim as ssim
+# from utils import imread_CS_py, img2col_py, col2im_CS_py, psnr, add_test_noise, write_data,get_cond
+from torch.utils.data import Dataset, DataLoader
+from torch.utils.tensorboard import SummaryWriter
+
+from model import *
+
+parser = ArgumentParser(description='ISTA-Net-plus-plus')
+
+parser.add_argument('--start_epoch', type=int, default=0, help='epoch number of start training')
+parser.add_argument('--end_epoch', type=int, default=400, help='epoch number of end training')
+parser.add_argument('--layer_num', type=int, default=20, help='phase number of ISTA-Net-plus')
+parser.add_argument('--learning_rate', type=float, default=1e-4, help='learning rate')
+parser.add_argument('--group_num', type=int, default=1, help='group number for training')
+parser.add_argument('--cs_ratio', type=int, default=50, help='from {1, 4, 10, 25, 40, 50}')
+parser.add_argument('--gpu_list', type=str, default='0', help='gpu index')
+parser.add_argument('--data_dir', type=str, default='cs_train400_png', help='training data directory')
+parser.add_argument('--rgb_range', type=int, default=1, help='value range 1 or 255')
+parser.add_argument('--n_channels', type=int, default=1, help='1 for gray, 3 for color')
+parser.add_argument('--patch_size', type=int, default=33, help='from {1, 4, 10, 25, 40, 50}')
+
+parser.add_argument('--matrix_dir', type=str, default='sampling_matrix', help='sampling matrix directory')
+parser.add_argument('--model_dir', type=str, default='model', help='trained or pre-trained model directory')
+parser.add_argument('--data_dir_org', type=str, default='data', help='training data directory')
+parser.add_argument('--log_dir', type=str, default='log', help='log directory')
+parser.add_argument('--ext', type=str, default='.png', help='training data directory')
+
+parser.add_argument('--result_dir', type=str, default='result', help='result directory')
+parser.add_argument('--test_name', type=str, default='Set11', help='name of test set')
+parser.add_argument('--test_cycle', type=int, default=10, help='epoch number of each test cycle')
+
+args = parser.parse_args()
+
+localtime = time.localtime(time.time())
+nowtime = str(localtime.tm_year) + '_' + str(localtime.tm_mon) + '_' + str(localtime.tm_mday) + '_' + str(
+    localtime.tm_hour) + '_' + str(localtime.tm_min) + '_' + str(localtime.tm_sec)
+
+tensorBoardPath = './singlePixel_ISTANetpp/%s' % nowtime
+if not os.path.exists(tensorBoardPath):
+    os.makedirs(tensorBoardPath)
+writer = SummaryWriter('%s/' % tensorBoardPath)
+
+start_epoch = args.start_epoch
+end_epoch = args.end_epoch
+learning_rate = args.learning_rate
+layer_num = args.layer_num
+group_num = args.group_num
+cs_ratio = args.cs_ratio
+gpu_list = args.gpu_list
+
+y_size = 5000
+gamma = torch.Tensor([[y_size / 16384]])
+
+n_input = 5000
+n_output = 16384
+nrtrain = 780
+batch_size = 8
+
+Phi_data_Name = './%s/A.mat' % args.matrix_dir  # 8000/16384
+Phi_data = sio.loadmat(Phi_data_Name)
+Phi_input = Phi_data['A']
+
+Training_data_Name = 'X0.mat'
+Training_data = sio.loadmat('./%s/%s' % (args.data_dir, Training_data_Name))
+Training_labels = Training_data['X']
+
+Y_data_Name = 'Y0.mat'
+Y_data = sio.loadmat('./%s/%s' % (args.data_dir, Y_data_Name))
+Y_data = Y_data['Y']
+
+Qinit_Name = './%s/Q_init_316.mat' % args.matrix_dir
+
+if os.path.exists(Qinit_Name):
+    Qinit_data = sio.loadmat(Qinit_Name)
+    Qinit = Qinit_data['Qinit']
+
+else:
+    X_data = Training_labels.transpose()
+    # Y_data = np.dot(Phi_input, X_data)
+    Y_YT = np.dot(Y_data, Y_data.transpose())
+    X_YT = np.dot(X_data, Y_data.transpose())
+    Qinit = np.dot(X_YT, np.linalg.inv(Y_YT))
+    del X_data, X_YT, Y_YT
+    sio.savemat(Qinit_Name, {'Qinit': Qinit})
+
+Y_data = Y_data.transpose()
+Training_labels = np.concatenate((Training_labels, Y_data), axis=1)
+# Valid_labels = Training_labels[nrtrain:,:]
+Training_labels = Training_labels[:nrtrain, :]
+
+Phi = torch.from_numpy(Phi_input).type(torch.FloatTensor)
+Phi = Phi.cuda()
+
+Qinit = torch.from_numpy(Qinit).type(torch.FloatTensor)
+Qinit = Qinit.cuda()
+
+
+class RandomDataset(Dataset):
+    def __init__(self, data, length):
+        self.data = data
+        self.len = length
+
+    def __getitem__(self, index):
+        return torch.Tensor(self.data[index, :]).float()
+
+    def __len__(self):
+        return self.len
+
+
+rand_loader = DataLoader(dataset=RandomDataset(Training_labels, nrtrain), batch_size=batch_size, num_workers=2,
+                         shuffle=True)
+
+model = ISTA_Netpp(layer_num, n_output)
+model = nn.DataParallel(model)
+model = model.cuda()
+
+optimizer = torch.optim.Adam(model.parameters(), lr=learning_rate)
+
+model_dir = "./%s/CS_ISTA_Net_pp_layer_%d_lr_%.4f_size_%d_time_%s" % (
+    args.model_dir, layer_num, learning_rate, y_size, nowtime)
+
+log_file_name = "./%s/Log_CS_ISTA_Net_pp_layer_%d_lr_%.4f_size_%d_time_%s.txt" % (
+    args.log_dir, layer_num, learning_rate, y_size, nowtime)
+
+if not os.path.exists(model_dir):
+    os.makedirs(model_dir)
+
+j = 0
+for epoch_i in range(start_epoch + 1, end_epoch + 1):
+    for data in rand_loader:
+        x = data[:, :16384].cuda()
+        y = data[:, 16384:].cuda()
+
+        x_output = model(y, gamma, Phi, n_input)
+
+        Prediction_value = x_output.cpu().data.numpy()
+
+        X_rec = np.reshape(Prediction_value, (-1, 128, 128))
+        X_ori = np.reshape(x.cpu().data.numpy(), (-1, 128, 128))
+
+        IMG = np.concatenate((np.clip(X_ori[0], 0, 1), np.clip(X_rec[0], 0, 1)), axis=1)
+        writer.add_image('IMG', IMG, global_step=j, dataformats='HW')
+
+        loss = torch.mean(torch.pow(x_output - x), 2)
+        writer.add_scalar('loss_all', loss, global_step=j)
+        j += 1
+
+        optimizer.zero_grad()
+        loss.backward()
+        optimizer.step()
+
+        output_data = "[%02d/%02d] Total Loss: %.8f\n" % (epoch_i, end_epoch, loss.item())
+        print(output_data)
+
+    output_file = open(log_file_name, 'a')
+    output_file.write(output_data)
+    output_file.close()
+
+    if epoch_i % 5 == 0:
+        torch.save(model.state_dict(), "./%s/net_params_%d.pkl" % (model_dir, epoch_i))  # save only the parameters
+writer.close()
